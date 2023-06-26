5fc3a35346efd2def4105c37b8fb61565037cfd4
sathya4509 <120151684+sathya4509@users.noreply.github.com>
2023-06-26 22:16:40 +0530

    React table initial commit

---
+++ src/components/columns.js
@@ -0,0 +1,23 @@
+
+
+export const COLUMNS = [
+    {
+        Header:'Id',
+        Accessor: 'id'
+    },
+    {
+        Header:'First Name',
+        Accessor: 'first_name'
+    },
+    {
+        Header:'Last Name',
+        Accessor: 'last_name'
+    },
+    {
+        Header:'Email',
+        Accessor: 'email'
+    }
+
+
+]
+ 
